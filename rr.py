919c989eebf6f1c83386bcc8005e153daa4fea77
Andrea Pozzato <andrea@HAL9000.local>
2014-05-01 21:33:11 +0200

    Reboot netgear router from shell

---
+++ rr.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#Reboot your netgear router
+__author__ = 'pzzt'
+
+import getpass
+import urllib2
+# import subprocess
+
+ip=raw_input("ip:")
+user=raw_input("user:")
+psw=getpass.getpass("password:")
+
+# def lookup(ip):
+#
+#     # Devo trovare il modo di avere una funzione che mi restituisca 1 o 0
+#     # a seconda se l'host è online o meno possibilmente senza dovermi scrivere una funzione
+#     # ping a mano. subprocess sostituisce la lib os.
+#
+#     output = subprocess.Popen(["ping", "-o", "-c3", "-W3000", ip],stdout=subprocess.PIPE).communicate()[0]
+#     print(output)
+#     if ("" in output):
+#         print(ip+"is offline!")
+
+def router_reboot(ip,user,psw):
+    url="http://"+ip+"/"
+    passman=urllib2.HTTPPasswordMgrWithDefaultRealm()
+    passman.add_password(None,url,user,psw)
+    urllib2.install_opener(urllib2.build_opener(urllib2.HTTPBasicAuthHandler(passman)))
+    comando=url+"setup.cgi?todo=reboot"
+    print(comando)
+    req=urllib2.urlopen(comando)
+
+router_reboot(ip,user,psw)
